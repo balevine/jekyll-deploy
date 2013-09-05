ddeaaccf934f5c2ffbca26215b858b62296d92b6
Brian Levine <balevine@gmail.com>
2013-09-04 22:57:48 -0700

    Change 'site' to 'master' branch for User Page deploy.

--- blog-deploy.sh
+++ blog-deploy.sh
@@ -23,25 +23,25 @@ tmp_dir="temp_$last_SHA"
 mkdir ~/$tmp_dir
 cp -r ./_site/* ~/$tmp_dir
 
-# Switch to the Jekyll branch, in this case 'site'
-git checkout site > /dev/null 2>&1
+# Switch to the Jekyll branch, in this case 'master'
+git checkout master > /dev/null 2>&1
 if [ $? -eq 1 ]
 then
   # Branch does not exist. Create an orphan branch.
-  git checkout --orphan site > /dev/null 2>&1
+  git checkout --orphan master > /dev/null 2>&1
   git add .
   git commit -m "New branch" > /dev/null 2>&1
   echo "New site branch created"
 fi
 
-# Remove the current contents of the 'site' branch and
+# Remove the current contents of the 'master' branch and
 # replace them with the contents of the temp folder
 current_dir=${PWD}
 rm -r *
 git rm -r --cached * > /dev/null 2>&1
 cp -r ~/$tmp_dir/* $current_dir
 
-# Commit the changes to 'site'
+# Commit the changes to 'master'
 message="Updated built site from 'source' - $last_SHA"
 git add .
 git commit -m "$message" > /dev/null 2>&1
@@ -50,7 +50,7 @@ git commit -m "$message" > /dev/null 2>&1
 rm -r ~/$tmp_dir
 
 # Push new site to server
-git push origin site > /dev/null 2>&1
+git push origin master > /dev/null 2>&1
 
 # Switch back to source
 git checkout source > /dev/null 2>&1
