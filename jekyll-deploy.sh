1a2dad9a65dcf5e8af80de8a5c03bb8fa6ff3111
Brian Levine <balevine@gmail.com>
2013-09-13 00:29:42 -0400

    Change branch names to variables

--- jekyll-deploy.sh
+++ jekyll-deploy.sh
@@ -3,7 +3,7 @@
 # A script to build a Jekyll site
 
 # Make sure you're on the 'source' branch
-git checkout source > /dev/null 2>&1
+git checkout $source > /dev/null 2>&1
 
 # Build the Jekyll site
 jekyll build > /dev/null 2>&1
@@ -35,25 +35,25 @@ tmp_dir="temp_$last_SHA"
 mkdir ~/$tmp_dir
 cp -r ./_site/* ~/$tmp_dir
 
-# Switch to the Jekyll branch, in this case 'master'
-git checkout master > /dev/null 2>&1
+# Switch to the Jekyll branch
+git checkout $built > /dev/null 2>&1
 if [ $? -eq 1 ]
 then
   # Branch does not exist. Create an orphan branch.
-  git checkout --orphan master > /dev/null 2>&1
+  git checkout --orphan $built > /dev/null 2>&1
   git add .
   git commit -m "New branch" > /dev/null 2>&1
   echo "New site branch created"
 fi
 
-# Remove the current contents of the 'master' branch and
+# Remove the current contents of the built branch and
 # replace them with the contents of the temp folder
 current_dir=${PWD}
 rm -r *
 git rm -r --cached * > /dev/null 2>&1
 cp -r ~/$tmp_dir/* $current_dir
 
-# Commit the changes to 'master'
+# Commit the changes to the built branch
 message="Updated built site from 'source' - $last_SHA"
 git add .
 git commit -m "$message" > /dev/null 2>&1
@@ -62,10 +62,10 @@ git commit -m "$message" > /dev/null 2>&1
 rm -r ~/$tmp_dir
 
 # Push new site to server
-git push origin master > /dev/null 2>&1
+git push origin $built > /dev/null 2>&1
 
 # Switch back to source
-git checkout source > /dev/null 2>&1
+git checkout $source > /dev/null 2>&1
 
 # Push the source to the server
-git push origin source > /dev/null 2>&1
+git push origin $source > /dev/null 2>&1
