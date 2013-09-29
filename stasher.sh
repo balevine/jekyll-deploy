07eb0ffdbf3b2c3f04eb3f0fe1a0726d8c4e1e5e
Brian Levine <balevine@gmail.com>
2013-09-29 19:32:30 -0400

    Get that stasher stashing

--- stasher.sh
+++ stasher.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # Look for uncommitted work
-status=$(git status --short > /dev/null)
-if [ $status -eq 0 ]
-then
-  echo "Working directory is clean!"
+status=$(git status)
+echo $status | grep -qF 'working directory clean'
+if [ $? = 0 ]; then
+  # The working directory is clean
+  # We can move on
+  exit 0
 else
-  echo "Working directory is not clean. Unclean! UNCLEAN!"
+  # The working directory is NOT clean
+  # We're gonna need to stash some shit
+  # Then we can move on
+  git stash
+
+  # Lets use the exit code 99
+  # This will mean there's something stashed
+  exit 99
 fi
